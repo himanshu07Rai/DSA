cba4a473680b0af55134cd40f051d6640585161e
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-02-05 18:48:28 +0530

    Time: 12 ms (11.99%), Space: 9.7 MB (22.61%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int ans;
+       
         int n = nums.size();
         int l=0,r=n;
+        int ans = n;
         while(l<r)
         {
             int m = l+(r-l)/2;
-            int val = nums[m];
-            // cout<<val<<"\n";
-            // if(val==target)
-            //     return m;
-            if(val>=target)
+            if(nums[m]==target)
+                return m;
+            if(nums[m]>=target)
             {
-                r=m;
-            }
+                r = m;
+            }   
             else l=m+1;
         }
         return l;
+        
     }
 };
