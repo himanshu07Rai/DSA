eed6ee1c7062a766cde518b45dcdfd502804cdc1
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-02-21 17:24:59 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.67%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char c:s)
+        {
+            if(!st.empty() && st.top()=='(' && c==')')
+                st.pop();
+            else if(!st.empty() && st.top()=='{' && c=='}')
+                st.pop();
+            else if(!st.empty() && st.top()=='[' && c==']')
+                st.pop();
+            else
+                st.push(c);
+        }
+        return st.empty();
+    }
+};
