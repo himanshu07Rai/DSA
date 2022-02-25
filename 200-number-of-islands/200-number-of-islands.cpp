8246d0eac7cd6681cdb8b7e1bba66656a1f22d18
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-02-25 16:20:00 +0530

    Time: 28 ms (98.59%), Space: 12.2 MB (84.59%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -4,29 +4,28 @@ public:
     {
         return i<n && i>=0 && j<m && j>=0;
     }
-    void countComponents(int i,int j,vector<vector<char>>& grid,vector<vector<bool>> &visited)
+    void countComponents(int i,int j,vector<vector<char>>& grid)
     {
         int n = grid.size(),m = grid[0].size();
-        visited[i][j] = true;
+        grid[i][j] = '0';
         int forI[]={0,-1,0,1},forJ[] = {-1,0,1,0};
         for(int k=0;k<4;k++)
         {
             int new_i = i+forI[k],new_j = j+forJ[k];
-            if(isValid(new_i,new_j,n,m) && !visited[new_i][new_j]&&grid[i][j]=='1' )
-                countComponents(new_i,new_j,grid,visited);
+            if(isValid(new_i,new_j,n,m) && grid[new_i][new_j]=='1' )
+                countComponents(new_i,new_j,grid);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(),m = grid[0].size();
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
         int ans = 0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1' && !visited[i][j])
+                if(grid[i][j]=='1')
                 {
-                   countComponents(i,j,grid,visited); 
+                   countComponents(i,j,grid); 
                     ans++;
                 }                   
                 
