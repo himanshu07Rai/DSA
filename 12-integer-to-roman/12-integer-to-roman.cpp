0b89b68921189c5bceb82e3f949de30fb3a80c95
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-03-01 18:55:01 +0530

    Time: 15 ms (40.43%), Space: 9.5 MB (19.19%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -3,15 +3,15 @@ public:
     string intToRoman(int n) {
         vector<pair<int, string>> m = {{1000, "M"},{900, "CM"} , {500, "D"},{400, "CD"} , {100, "C"} , {90, "XC"}, {50, "L"}, {40, "XL"},{10, "X"}, {9, "IX"},{5, "V"} , {4, "IV"},{1, "I"} };
         string ans = "";
-        while(n > 0){
+        // while(n > 0){
             for(auto x : m){
-                if(n >= x.first){
+                while(n >= x.first){
                     ans += x.second;
                     n -= x.first;
-                    break;
+                    // break;
                 }
             }
-        }
+        // }
         
         return ans;
     }
