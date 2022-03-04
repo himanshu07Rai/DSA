97566a98f94bf3e3be259b555df16d84a9b8206e
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-03-04 15:49:27 +0530

    Time: 4 ms (47.11%), Space: 6 MB (32.64%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while(x)
+        {
+            if(res>INT_MAX/10 || res<INT_MIN/10)
+            {
+                return 0;
+            }
+            int dig = x%10;
+            x/=10;
+            res=res*10+dig;
+        }
+        return res;
+    }
+};
