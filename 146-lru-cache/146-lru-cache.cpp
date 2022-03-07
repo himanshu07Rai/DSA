cbd9541bee46530ac777e70d520c77d6536b5a8d
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-03-07 11:39:02 +0530

    Time: 602 ms (56.40%), Space: 165.2 MB (68.82%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,31 +1,37 @@
 class LRUCache {
- int capacity;
-    list<pair<int, int>> li;
-    unordered_map<int, list<pair<int, int>>::iterator> um; 
-
+    unordered_map<int,list<pair<int,int>>::iterator> mymap;
+    list<pair<int,int>> mylist;
+    int c = 0,cap;
 public:
-    LRUCache(int capacity) : capacity{capacity} {}
+    
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
     
     int get(int key) {
-        if (um.find(key) == um.end()) return -1;
-        li.splice(li.begin(), li, um[key]);
-        return um[key]->second;
+        if(mymap.find(key)==mymap.end())
+            return -1;
+        mylist.splice(mylist.begin(),mylist,mymap[key]);
+        return mymap[key]->second;
     }
     
     void put(int key, int value) {
-        if (get(key) != -1) {
-            um[key]->second = value;
+        if(get(key)!=-1)
+        {
+            mymap[key]->second = value;
+            mylist.splice(mylist.begin(),mylist,mymap[key]);
             return;
         }
-        
-        if (um.size() == capacity) {
-            int delKey = li.back().first;
-            li.pop_back();
-            um.erase(delKey);
+        if(c==cap)
+        {
+            int delKey = mylist.back().first;
+            mylist.pop_back();
+            mymap.erase(delKey);
+            c--;
         }
-        
-        li.emplace_front(key, value);
-        um[key] = li.begin();
+        c++;
+        mylist.push_front({key,value});
+        mymap[key] = mylist.begin();
     }
 };
 
