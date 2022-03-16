fd32de0ac448482892e8ccf0435f22229e25a354
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-03-16 19:26:35 +0530

    Time: 23 ms (9.87%), Space: 15.5 MB (15.68%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -3,15 +3,14 @@ public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         stack<int> st;
         int i=0,j=0;
-        int n1 = pushed.size(),n2=popped.size();
+        int n = pushed.size();
         if(pushed[i]!=popped[j])
             while(pushed[i]!=popped[j])
             st.push(pushed[i++]);     
         
         
-        while(i<n1)
+        while(i<n)
         {
-            // cout<<i<<" ";
             while(!st.empty() && st.top()==popped[j])
             {
                 st.pop();
@@ -28,13 +27,6 @@ public:
             j++;
         }
         
-        // cout<<"\n";
-        
-        // while(!st.empty())
-        // {
-        //     cout<<st.top()<<" ";
-        //     st.pop();
-        // }
         return st.empty();
     }
 };
