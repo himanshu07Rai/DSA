db09e89524967f85f72feeabc6eaabb1c84631b9
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-03-30 15:38:37 +0530

    Added solution - LeetHub

--- Detect Loop in linked list - GFG/detect-loop-in-linked-list.cpp
+++ Detect Loop in linked list - GFG/detect-loop-in-linked-list.cpp
@@ -48,15 +48,13 @@ class Solution
     //Function to check if the linked list has a loop.
     bool detectLoop(Node* head)
     {
-        unordered_map<Node*,bool> m;
-        while(head)
+        Node* fast = head,*slow=head;
+        while(fast && fast->next)
         {
-            if(m.find(head)==m.end())
-            {
-                m[head] = true;
-                head = head->next;
-            }
-            else return true;
+           fast = fast->next->next;
+           slow = slow->next;
+           if(fast==slow)
+            return true;
         }
         return false;
     }
