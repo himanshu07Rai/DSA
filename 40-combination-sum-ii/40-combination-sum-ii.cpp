91c194dc3c9be3f709e1101e9c4b30e808ad375c
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-09 19:07:04 +0530

    Time: 17 ms (26.27%), Space: 10.6 MB (71.91%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     void generate(vector<int> &candidates,int start,vector<int> &curr,int target,vector<vector<int>>&ans)
-    {          
+    {
         if(target==0)
         {
             ans.push_back(curr);
@@ -10,10 +10,8 @@ public:
         }
         for(int i=start;i<candidates.size();i++)
         {
-            if(i>start && candidates[i-1]==candidates[i])
-                continue;
-            if(candidates[i]>target)
-                break;
+            if(candidates[i]>target)break;
+            if(i>start && candidates[i]==candidates[i-1]) continue;
             curr.push_back(candidates[i]);
             generate(candidates,i+1,curr,target-candidates[i],ans);
             curr.pop_back();
