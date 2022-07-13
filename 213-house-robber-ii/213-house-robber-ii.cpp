9e279f24d36102257a532db831f06f83218492dc
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-13 15:09:30 +0530

    Time: 4 ms (38.68%), Space: 8 MB (18.56%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>& nums) {
-        int n = nums.size(); 
-        int x = 0;
-        int y = nums[0],z;
-        for(int i=2;i<=n;i++)
+    int solve(vector<int>& nums,int l) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = 0;
+        dp[1] = nums[l];
+        for(int i=2;i<n;i++)
         {
-            int pick = nums[i-1];
-            if(i>1)
-            {
-                pick+=x;
-            }
-            int notPick = y;
-            z = max(pick,notPick);
-            x=y;
-            y=z;
+            int pick = nums[l+i-1]+dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = max(pick,notPick);
         }
-        return y;
+        return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1)
             return nums[0];
-        vector<int> woLast,woFirst;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(i)
-                woFirst.push_back(nums[i]);
-            if(i!=n-1)
-                woLast.push_back(nums[i]);
-        }
-        return max(solve(woFirst),solve(woLast));
+        if(n==2)
+            return max(nums[0],nums[1]);
+        // cout<<solve(nums,0,n-2)<<" "<<solve(nums,1,n-1);
+        return max(solve(nums,0),solve(nums,1));
     }
 };
