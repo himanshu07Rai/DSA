7038b0acfb1b49a8abad72efa9661223b3e76e3c
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-13 14:08:28 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -14,7 +14,18 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();  
-        vector<int>dp(n+1,-1);
-        return solve(n,nums,dp);
+        vector<int>dp(n+1,0);
+        dp[1] = nums[0];
+        for(int i=2;i<=n;i++)
+        {
+            int pick = nums[i-1];
+            if(i>1)
+            {
+                pick+=dp[i-2];
+            }
+            int notPick = dp[i-1];
+            dp[i] = max(pick,notPick);
+        }
+        return dp[n];
     }
 };
