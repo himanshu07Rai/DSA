e7278260feae26a8eb255974fad45a7923f18f01
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-14 12:16:31 +0530

    Time: 2 ms (49.32%), Space: 6.5 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    int countPaths(int i,int j,int n,int m,vector<vector<int>> &dp)
+    int countPaths(int i,int j,vector<vector<int>> &dp)
     {
-        if(i==n-1 && j==m-1)
+        if(i==0 && j==0)
             return 1;
-        if(i==n || j==m)
-            return 0;
+        
         if(dp[i][j]!=-1)
             return dp[i][j];
-        return dp[i][j]=countPaths(i+1,j,n,m,dp)+countPaths(i,j+1,n,m,dp);
+        
+        if(i<0 || j<0)
+            return 0; 
+        int up = countPaths(i-1,j,dp);
+        int left = countPaths(i,j-1,dp);
+        return dp[i][j]=up+left;
+        
     }
     int uniquePaths(int m, int n) {
         if(m==1 && n==1)
             return 1;
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        countPaths(0,0,m,n,dp);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                    dp[i][j] = 1;
+                else{
+                    int up = 0,left = 0;
+                    if(i>0)up = dp[i-1][j];
+                    if(j>0)left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        // return countPaths(m-1,n-1,dp);
 
-        return dp[0][0];
+        return dp[m-1][n-1];
     }
 };
