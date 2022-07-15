16570716ca45a60432f9102973c814b3c01c13f7
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-15 15:50:05 +0530

    Time: 25 ms (69.80%), Space: 26.4 MB (61.44%) - LeetHub

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(vector<int>& postorder, int low,int high,int &i,unordered_map<int,int>& um) {
+        // if(low>high)return NULL;
+        // int loc = um[postorder[r]];
+        // TreeNode *temp = new TreeNode(postorder[r]);
+        // r++;
+        // temp->left = helper(postorder,low,loc-1,r,um);
+        // temp->right = helper(postorder,loc+1,high,r,um);
+        // return temp;
+        if(low>high)
+            return NULL;
+        int loc = um[postorder[i]];
+        TreeNode *temp= new TreeNode(postorder[i]);
+        i--;
+        temp->right=helper(postorder,loc+1,high,i,um); 
+        temp->left=helper(postorder,low,loc-1,i,um);
+        return temp;
+        
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        // sort(postorder.begin(),postorder.end());
+        int n = inorder.size();
+        unordered_map<int,int> um;
+        
+        for(int i=0;i<n;i++)
+            um[inorder[i]] = i;
+        
+        int i = n-1;
+        int low = 0,high = n-1;
+        return helper(postorder,low,high,i,um);
+    }
+};
