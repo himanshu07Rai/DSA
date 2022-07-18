896598e05674d832b988ddba6d63bf0209334b5a
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-18 15:11:27 +0530

    Time: 192 ms (59.15%), Space: 83.8 MB (54.48%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -4,10 +4,11 @@ public:
         int ans = 0;
         int sum = 0;
         unordered_map<int,int> m;
-        m[0] = -1;
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i]==0?-1:1;
+            if(sum==0)
+                ans = max(ans,i+1);
             auto it = m.find(sum);
             if(it!=m.end())
             {
