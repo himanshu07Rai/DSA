6b857301de4640ad9945d7a098375fe3eb254b11
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-19 15:57:37 +0530

    Time: 151 ms (62.87%), Space: 76.8 MB (5.93%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int solve(int n,vector<int>& nums,vector<int>&dp)
     {
-        if(n==0)
-            return -1e7;
         if(dp[n]!=-1)
             return dp[n];
-        int append = solve(n-1,nums,dp)+nums[n-1];
-        int New = nums[n-1];
+        if(n==0)
+            return dp[n] = nums[0];
+        
+        int append = solve(n-1,nums,dp)+nums[n];
+        int New = nums[n];
         return dp[n]= max(append,New);
         
     }
@@ -15,11 +16,11 @@ public:
         int maxSum = -1e7;
         int n = nums.size();
         int sum = nums[0];
-        vector<int>dp(n+1,-1);
-        int ans = solve(n,nums,dp);
+        vector<int>dp(n,-1);
+        int ans = solve(n-1,nums,dp);
         // for(int i:dp)
         //     cout<<i<<" ";
-        dp[0] = -1e7;
+        // dp[0] = -1e7;
         return *max_element(dp.begin(),dp.end());
     }
 };
