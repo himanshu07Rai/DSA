6e42b88b1547cd33c93848b13512ccaf56a0bd23
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-19 16:11:34 +0530

    Time: 129 ms (82.60%), Space: 67.8 MB (53.44%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int solve(int n,vector<int>& nums,vector<int>&dp)
-    {
-        if(dp[n]!=-1)
-            return dp[n];
-        if(n==0)
-            return dp[n] = nums[0];
-        
-        int append = solve(n-1,nums,dp)+nums[n];
-        int New = nums[n];
-        return dp[n]= max(append,New);
-        
-    }
     int maxSubArray(vector<int>& nums) {
         int maxSum = -1e7;
         int n = nums.size();
-        int sum = nums[0];
-        vector<int>dp(n,-1);
-        int ans = solve(n-1,nums,dp);
-        // for(int i:dp)
-        //     cout<<i<<" ";
-        // dp[0] = -1e7;
-        return *max_element(dp.begin(),dp.end());
+        int sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            sum = sum+nums[i];
+            maxSum = max(maxSum,sum);
+            sum = max(0,sum);
+        }
+        return maxSum;
     }
 };
