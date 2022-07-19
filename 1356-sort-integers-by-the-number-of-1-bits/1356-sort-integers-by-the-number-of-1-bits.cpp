5b9df1379b33f335a76b92bb50ac39b0586c2d78
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-19 15:09:17 +0530

    Time: 20 ms (30.05%), Space: 10 MB (89.01%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    static int getOnes(int n)
-    {
-        int c = 0;
-        while(n)
-        {
-            c+=n%2;
-            n = n/2;
-        }
-        return c;
-    }
+    // static unsigned int getOnes(int n)
+    // {
+    //     int c = 0;
+    //     while(n)
+    //     {
+    //         c+=n%2;
+    //         n>>1;
+    //     }
+    //     return c;
+    // }
     static bool comp(int &a,int &b)
     {
-        if(getOnes(a) != getOnes(b)){
-            return getOnes(a) < getOnes(b);
-        }
-        else  return a < b;
+        if(__builtin_popcount(a) < __builtin_popcount(b))
+            return true;
+        if(__builtin_popcount(a) == __builtin_popcount(b))
+            return a<b;
+        return false;
     }
     vector<int> sortByBits(vector<int>& arr) {
         sort(arr.begin(),arr.end(),comp);
