2adebc3c07e066c393ce7b68cdcb1d9bb6561c78
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-27 17:00:47 +0530

    Time: 6 ms (61.09%), Space: 10.1 MB (20.27%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int lastDay = days[days.size()-1];
-        vector<int> dp(lastDay+1); //size till the last travel day is enough 
-        unordered_set<int> travel(days.begin(), days.end()); //store travel days in hashset
-
+        
+        int n = days.size();
+        int lastDay = days[n-1];
+        vector<int> dp(lastDay+1);
         dp[0] = 0;
-
+        unordered_set<int> travelDays(days.begin(),days.end());
         for(int i=1;i<=lastDay;i++)
         {
-            if(!travel.count(i)) //if 'i' is not a travel day 
+            if(travelDays.count(i)==0)
             {
-                dp[i] = dp[i-1]; //cost till day 'i' would be same as previous day cost i.e 'i-1'
+                dp[i]=dp[i-1];
                 continue;
             }
-             //if 'i' is a travel day then we have 3 cases    
-            dp[i] = costs[0] + dp[i-1]; //for single day ticket
-            dp[i] = min(costs[1] + dp[max(i-7, 0)], dp[i]); //for 7 day ticket
-            dp[i] = min(costs[2] + dp[max(i-30, 0)], dp[i]); //for 30 day ticket
-
+            dp[i] = dp[i-1]+costs[0];
+            int j = max(0,i-7);
+            dp[i] = min(dp[i],costs[1]+dp[j]);
+            j = max(0,i-30);
+            dp[i] = min(dp[i],costs[2]+dp[j]);
         }
-        return dp[lastDay]; //return min cost upto the last travel day
+        
+        return dp[lastDay];
     }
     
 };
