a7223e22d29f3292ce3ae017c82a43244396761e
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-07-30 22:30:12 +0530

    Time: 812 ms (17.80%), Space: 10.8 MB (20.87%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size(),ans = 1;
-        vector<int>dp(n,1);
+        int n = nums.size(),Max = 1,lastIdx=0;
+        vector<int>dp(n,1),map(n,0);
+            
         for(int i=0;i<n;i++)
         {
+            map[i] = i;
             for(int prev=0;prev<i;prev++)
             {
-                if(nums[prev]<nums[i])
-                    dp[i] = max(dp[i],1+dp[prev]);
+                if(nums[prev]<nums[i] && 1+dp[prev]>dp[i])
+                {
+                    dp[i] = 1+dp[prev];
+                    map[i] = prev;
+                }                    
             }
-            ans = max(ans,dp[i]);
+            if(dp[i]>Max)
+            {
+                Max = dp[i];
+                lastIdx = i;
+            }
         }
-        return ans;
+        vector<int> temp;
+        temp.push_back(nums[lastIdx]);
+        while(map[lastIdx]!=lastIdx)
+        {
+            lastIdx = map[lastIdx];
+            temp.push_back(nums[lastIdx]);
+        }
+        reverse(temp.begin(),temp.end());
+        for(int i:temp)
+            cout<<i<<" ";
+        return Max;
     }
 };
