793384a4118c93ca5745323ff9371c575f2597e3
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-08-02 12:21:47 +0530

    Time: 14 ms (85.19%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size(),Max = 1,lastIdx=0;
-        vector<int>dp(n,1),parent(n,0);
-        int ans = 1;
-        for(int i=0;i<n;i++)
+        int n = nums.size(),ans = 1;
+        vector<int> v;
+        v.push_back(nums[0]);
+        for(int i=1;i<n;i++)
         {
-            parent[i]=i;
-            for(int prev = 0;prev<i;prev++)
-            {
-                if(nums[i]>nums[prev])
-                {
-                    if(dp[prev]+1>dp[i])
-                    {
-                        dp[i] = 1+dp[prev];
-                        parent[i]=prev;
-                    }
-                }
-            }
-            if(dp[i]>ans)
-            {
-                ans = dp[i];
-                lastIdx=i;
+            if(nums[i]>v.back())
+                v.push_back(nums[i]),ans++;
+            else{
+                int idx = lower_bound(v.begin(),v.end(),nums[i])-v.begin();
+                v[idx] = nums[i];
             }
         }
-        vector<int>temp;
-        temp.push_back(nums[lastIdx]);
-        while(parent[lastIdx]!=lastIdx)
-        {
-            lastIdx = parent[lastIdx];
-            temp.push_back(nums[lastIdx]);
-        }
-        
-        reverse(temp.begin(),temp.end());
-        for(int i:temp)
-        {
-            cout<<i<<" ";
-        }
-        cout<<"\n";
         return ans;
     }
 };
