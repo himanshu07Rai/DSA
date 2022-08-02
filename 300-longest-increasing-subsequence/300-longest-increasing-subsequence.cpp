0052c116b102a1d1acd090a72d82e5d76bdcc90a
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-08-02 11:23:54 +0530

    Time: 2463 ms (5.00%), Space: 292.3 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,36 +1,50 @@
 class Solution {
 public:
+    int solve(int i,int prev,vector<int>&nums,vector<vector<int>>&dp)
+    {
+        if(i==nums.size())
+            return 0;
+        if(dp[i][prev+1]!=-1)
+            return dp[i][prev+1];
+        int notTake = solve(i+1,prev,nums,dp);
+        int take = INT_MIN;
+        if(prev==-1 || nums[i]>nums[prev])
+            take = 1+solve(i+1,i,nums,dp);
+        return dp[i][prev+1] = max(take,notTake);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size(),Max = 1,lastIdx=0;
-        vector<int>dp(n,1),map(n,0);
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return solve(0,-1,nums,dp);
+        // vector<int>dp(n,1),map(n,0);
             
-        for(int i=0;i<n;i++)
-        {
-            map[i] = i;
-            for(int prev=0;prev<i;prev++)
-            {
-                if(nums[prev]<nums[i] && 1+dp[prev]>dp[i])
-                {
-                    dp[i] = 1+dp[prev];
-                    map[i] = prev;
-                }                    
-            }
-            if(dp[i]>Max)
-            {
-                Max = dp[i];
-                lastIdx = i;
-            }
-        }
-        vector<int> temp;
-        temp.push_back(nums[lastIdx]);
-        while(map[lastIdx]!=lastIdx)
-        {
-            lastIdx = map[lastIdx];
-            temp.push_back(nums[lastIdx]);
-        }
-        reverse(temp.begin(),temp.end());
-        for(int i:temp)
-            cout<<i<<" ";
-        return Max;
+//         for(int i=0;i<n;i++)
+//         {
+//             map[i] = i;
+//             for(int prev=0;prev<i;prev++)
+//             {
+//                 if(nums[prev]<nums[i] && 1+dp[prev]>dp[i])
+//                 {
+//                     dp[i] = 1+dp[prev];
+//                     map[i] = prev;
+//                 }                    
+//             }
+//             if(dp[i]>Max)
+//             {
+//                 Max = dp[i];
+//                 lastIdx = i;
+//             }
+//         }
+//         vector<int> temp;
+//         temp.push_back(nums[lastIdx]);
+//         while(map[lastIdx]!=lastIdx)
+//         {
+//             lastIdx = map[lastIdx];
+//             temp.push_back(nums[lastIdx]);
+//         }
+//         reverse(temp.begin(),temp.end());
+//         for(int i:temp)
+//             cout<<i<<" ";
+//         return Max;
     }
 };
