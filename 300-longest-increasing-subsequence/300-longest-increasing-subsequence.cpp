ce1908bf401552429b67d91f61333221309dae85
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-08-02 11:51:09 +0530

    Time: 449 ms (43.42%), Space: 10.5 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,49 +2,17 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size(),Max = 1,lastIdx=0;
-        vector<int>next(n+1,0),curr(n+1,0);
-        for(int i=n-1;i>=0;i--)
+        vector<int>dp(n,1);
+        int ans = 1;
+        for(int i=0;i<n;i++)
         {
-            for(int prev=i;prev>=-1;prev--)
+            for(int prev = 0;prev<i;prev++)
             {
-                int notTake = next[prev+1];
-                int take = INT_MIN;
-                if(prev==-1 || nums[i]>nums[prev])
-                    take = 1+next[i+1];
-                curr[prev+1] = max(take,notTake);              
+                if(nums[i]>nums[prev])
+                    dp[i]=max(dp[i],1+dp[prev]);
             }
-            next = curr;
+            ans = max(ans,dp[i]);
         }
-        return next[0];
-        // vector<int>dp(n,1),map(n,0);
-            
-//         for(int i=0;i<n;i++)
-//         {
-//             map[i] = i;
-//             for(int prev=0;prev<i;prev++)
-//             {
-//                 if(nums[prev]<nums[i] && 1+dp[prev]>dp[i])
-//                 {
-//                     dp[i] = 1+dp[prev];
-//                     map[i] = prev;
-//                 }                    
-//             }
-//             if(dp[i]>Max)
-//             {
-//                 Max = dp[i];
-//                 lastIdx = i;
-//             }
-//         }
-//         vector<int> temp;
-//         temp.push_back(nums[lastIdx]);
-//         while(map[lastIdx]!=lastIdx)
-//         {
-//             lastIdx = map[lastIdx];
-//             temp.push_back(nums[lastIdx]);
-//         }
-//         reverse(temp.begin(),temp.end());
-//         for(int i:temp)
-//             cout<<i<<" ";
-//         return Max;
+        return ans;
     }
 };
