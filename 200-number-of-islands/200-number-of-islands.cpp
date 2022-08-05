fdc1faf7cbd06e611a09e4e9c7eb1c26fddd4c09
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-08-05 19:07:51 +0530

    Time: 49 ms (64.29%), Space: 12.5 MB (39.11%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    bool isValid(int i,int j,int n,int m)
+    bool isNeighbour(int i,int j,int n,int m)
     {
-        return i<n && i>=0 && j<m && j>=0;
+        return i>=0 && i<n && j>=0 && j<m;
     }
-    void countComponents(int i,int j,vector<vector<char>>& grid)
+    void dfs(vector<vector<char>>& grid,vector<vector<bool>> &visited, int i,int j,int n,int m)
     {
-        int n = grid.size(),m = grid[0].size();
-        grid[i][j] = '0';
-        int forI[]={0,-1,0,1},forJ[] = {-1,0,1,0};
+        visited[i][j]=true;
+        int for_i[]={0,-1,0,1},for_j[]={-1,0,1,0};
         for(int k=0;k<4;k++)
         {
-            int new_i = i+forI[k],new_j = j+forJ[k];
-            if(isValid(new_i,new_j,n,m) && grid[new_i][new_j]=='1' )
-                countComponents(new_i,new_j,grid);
+            int ni=i+for_i[k],nj=j+for_j[k];
+            if(isNeighbour(ni,nj,n,m) && grid[ni][nj]=='1' && !visited[ni][nj])
+                dfs(grid,visited,ni,nj,n,m);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(),m = grid[0].size();
-        int ans = 0;
-        for(int i=0;i<n;i++)
+        if(!n)
+            return 0;
+        int count = 0;
+        vector<vector<bool>> visited(n,vector<bool>(m));
+        for (int i = 0; i < n; i++)
         {
-            for(int j=0;j<m;j++)
+            for (int j = 0; j < m; j++)
             {
-                if(grid[i][j]=='1')
+                if (grid[i][j] == '1' && !visited[i][j])
                 {
-                   countComponents(i,j,grid); 
-                    ans++;
-                }                   
-                
+                    count++;
+                    dfs(grid, visited, i, j,n,m);
+                }
             }
         }
-        return ans;
         
+
+    return count;
     }
 };
