7f0f44cb2e6b7162fcb3822c38d50226649daeff
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-08-05 13:52:41 +0530

    Time: 14 ms (75.72%), Space: 11.2 MB (73.66%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -1,6 +1,5 @@
 class Solution {
-public:
-    static bool comp(pair<string,string>&item1,pair<string,string>&item2)
+  static bool comp(pair<string,string>&item1,pair<string,string>&item2)
     {
         if(item1.second!=item2.second)
             return item1.second<item2.second;
@@ -8,27 +7,29 @@ public:
             return item1.first<item2.first;
         }
     }
+    public:
+   
     vector<string> reorderLogFiles(vector<string>& logs) {
-        vector<pair<string,string>> let,dig;
+        vector<pair<string,string>> let;
+        vector<string>dig;
         for(string s:logs)
         {
             int i = s.find(' ');
             if(isalpha(s[i+1])){
                 let.push_back({s.substr(0,i),s.substr(i+1)});
             }else{
-                dig.push_back({s.substr(0,i),s.substr(i+1)});
+                dig.push_back(s);
             }
         }
         sort(let.begin(),let.end(),comp);
         vector<string> ans;
         for(auto s:let)
         {
-            // cout<<s.first<<" "<<s.second<<"\n";
             ans.push_back(s.first+" "+s.second);
         }
         for(auto s:dig)
         {
-            ans.push_back(s.first+" "+s.second);
+            ans.push_back(s);
         }
         return ans;
     }
