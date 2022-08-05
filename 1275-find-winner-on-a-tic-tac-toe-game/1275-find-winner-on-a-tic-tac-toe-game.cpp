fd4de16d7b9aee211d9f4d1cf07d1513d373248e
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-08-05 20:26:11 +0530

    Time: 11 ms (5.27%), Space: 8.2 MB (86.33%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string tictactoe(vector<vector<int>>& moves) {
         int rowA[3]={0},rowB[3]={0},colA[3]={0},colB[3]={0},d1A=0,d2A=0,d1B=0,d2B=0;
-        // int row[2][3]={},col[2][3]={},d1[2]={},d2[2]={},
         int id=0;
         for(auto i:moves){
             if(!id)
