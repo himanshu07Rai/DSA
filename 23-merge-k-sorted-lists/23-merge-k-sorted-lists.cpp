0891ee6b8f5c2e3aefeb4238f6275fd97bc942f2
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2022-09-13 01:54:43 +0530

    Time: 50 ms (43.65%), Space: 13.5 MB (39.92%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -14,7 +14,7 @@ public:
         int n = lists.size();
         // return lists[0];
         ListNode* dummy = new ListNode();
-        ListNode* temp=dummy;
+        ListNode* tail=dummy;
         priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
         
         for(int i=0;i<n;i++)
@@ -22,23 +22,19 @@ public:
             if(lists[i])
                 pq.push({lists[i]->val,lists[i]});
         }
-        // vector<int>re;
+        vector<int>re;
         
         while(!pq.empty())
         {
             auto t = pq.top();
-            // re.push_back(t.first);
             pq.pop();
-            ListNode* a = new ListNode(t.first);
-            temp->next = a;
-            temp = temp->next;
+            tail->next = t.second;
+            tail = tail->next;
             
             if(t.second->next)
                 pq.push({t.second->next->val,t.second->next});
                 
         }
-        // for(int i:re)
-        //     cout<<i<<" ";
         return dummy->next;
     }
 };
